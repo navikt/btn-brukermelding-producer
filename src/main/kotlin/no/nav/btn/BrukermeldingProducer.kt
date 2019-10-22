74145cfc63cca0fb343cf51b7695b42e29bc7667
Richard Borge <richard.borge@nav.no>
2019-10-22 17:51:46 +0200

    Fjerner serializer på constructor fordi det førte til nullpointer da de ikke var configurert riktig.

--- src/main/kotlin/no/nav/btn/BrukermeldingProducer.kt
+++ src/main/kotlin/no/nav/btn/BrukermeldingProducer.kt
@@ -4,18 +4,18 @@ import io.confluent.kafka.serializers.KafkaJsonSerializer
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.StringSerializer
 import org.slf4j.LoggerFactory
 import java.util.*
 
 private const val TOPIC = "btn-brukermelding"
 
 private val logger = LoggerFactory.getLogger("no.nav.btn.BrukermeldingProducer")
-private val producer: KafkaProducer<MessageKey, Message> = KafkaProducer(getProducerConfig(),
-        KafkaJsonSerializer<MessageKey>(), KafkaJsonSerializer<Message>())
+private val producer: KafkaProducer<String, Message> = KafkaProducer(getProducerConfig())
 
 fun sendMessageToKafka(message: Message) {
     logger.info("Sender melding ${message.message}")
-    producer.send(ProducerRecord(TOPIC, MessageKey(UUID.randomUUID()), message))
+    producer.send(ProducerRecord(TOPIC, UUID.randomUUID().toString(), message))
 }
 
 private fun getProducerConfig(): Properties {
@@ -24,5 +24,7 @@ private fun getProducerConfig(): Properties {
 
     props[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = config.serverUrl
     props[ProducerConfig.CLIENT_ID_CONFIG] = config.clientId
+    props[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+    props[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] =  KafkaJsonSerializer::class.java
     return props
 }
--- src/main/kotlin/no/nav/btn/Model.kt
+++ src/main/kotlin/no/nav/btn/Model.kt
@@ -1,7 +1,3 @@
 package no.nav.btn
 
-import java.util.*
-
-data class MessageKey(val uuid: UUID)
-
 data class Message(val message: String, val timestamp: Long)
